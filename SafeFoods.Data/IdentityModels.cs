42a6805a4f4670e769d7ecb6fb6b288da205e7bd
Michael Cotofan <mccotofan@gmail.com>
2021-07-10 11:01:05 -0400

    project set up + EF + identity models

--- SafeFoods.Data/IdentityModels.cs
+++ SafeFoods.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -28,6 +30,39 @@ namespace SafeFoods.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet<MealRecipe> MealRecipes { get; set; }
+        public DbSet<IngredientTag> IngredientTags { get; set; }
+
+        public DbSet<Nutrition> Nutritions { get; set; }
+        public DbSet<DessertRecipe> DessertRecipes { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+
+        public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+        {
+            public IdentityUserLoginConfiguration()
+            {
+                HasKey(iul => iul.UserId);
+            }
+        }
+
+        public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+        {
+            public IdentityUserRoleConfiguration()
+            {
+                HasKey(iur => iur.UserId);
+            }
         }
     }
 }
