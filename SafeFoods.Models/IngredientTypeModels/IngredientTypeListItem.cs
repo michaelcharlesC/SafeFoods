96f6533b7a11c7fce6ea3b96b1fce0b58efb3b2e
Michael Cotofan <mccotofan@gmail.com>
2021-07-12 13:48:00 -0400

    Added LISTITEM models for all

---
+++ SafeFoods.Models/IngredientTypeModels/IngredientTypeListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeFoods.Models.IngredientTagModels
+{
+    public class IngredientTypeListItem
+    {
+        public int IngredientTypeId { get; set; }
+        public string Name { get; set; }
+        public ICollection<IngredientTag> IngredientTagList { get; set; }
+    }
+}
