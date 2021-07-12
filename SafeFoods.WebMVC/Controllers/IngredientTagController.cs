10e3ab8f308ea2fff129ca3efbb231ec8cf2dd8f
Michael Cotofan <mccotofan@gmail.com>
2021-07-12 13:25:15 -0400

    Before creating RecipeListItem model

---
+++ SafeFoods.WebMVC/Controllers/IngredientTagController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SafeFoods.WebMVC.Controllers
+{
+    public class IngredientTagController : Controller
+    {
+        // GET: IngredientTag
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
